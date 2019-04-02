6333af96d0809224aea7f9eb15347ee1624e9742
meds <mehdi.herzi@gmail.com>
2019-04-02 13:47:42 +0200

    ok

---
+++ src/javaapplication7/Conteneur.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Ordi
+ */
+public class Conteneur {
+    
+    
+        private float largueur;
+        private float longueur;
+        private float profondeur;
+        private int   taille; 
+        
+        public Conteneur coffre [] = new  Conteneur[taille];     
+        
+        
+        public Conteneur(float largueur ,float longueur,float profondeur ){
+          float a = 0f;
+         this.largueur = largueur;  
+         this.longueur= longueur; 
+         this.profondeur = profondeur;
+         
+       kdl
+        }
+        
+        
+          public   String  toString()
+        {
+            String a= " largueur " + largueur+ " longueur "+longueur+" profondeur "+ profondeur;
+           return a;
+        }
+         
+          
+          public void tail()
+          {
+          
+          
+          }
+          public  Conteneur  mettre(Conteneur a,int taille,int i)
+          {  
+              
+              
+             
+                   this.coffre[i] = a;
+             
+            return this.coffre[i]; 
+           
+          }
+    
+    
+        
+        
+     public float  surface ()
+        {
+            return this.largueur*this.longueur;
+        }
+    
+}
---
+++ src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Ordi
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+
+
+    public static void main(String[] args) {
+       
+        Ville a;
+        a= new Ville();
+      
+        Conteneur voiture = new Conteneur(0,0,0);
+                Conteneur bloc = new Conteneur(2,0,0);
+        
+          
+        
+       voiture.mettre(bloc,10,0); 
+        
+  
+            System.out.println(voiture.coffre[0].toString());
+            
+        
+ 
+        
+        
+        
+  
+    }
+
+    
+}
