cc7312641e5d09a5bf05e55bdaead5c587761c67
meds <mehdi.herzi@gmail.com>
2019-04-04 10:09:00 +0200

    Create vehicule.java

---
+++ src/javaapplication7/vehicule.java
@@ -0,0 +1,27 @@
+
+
+
+
+package javaapplication7;
+
+public class test{
+
+  
+
+   public float alpha;
+
+   public test(float alpha){
+   
+    this.alpha = alpha;
+    
+   }
+   
+   
+    public String toString()
+    {
+        
+        return " le alpha "+this.alpha;
+    }
+
+
+};
